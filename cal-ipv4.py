704648e055ccd7daafd5f758eb0a7e3c6fe206d3
jd correa tavares <jd.correa.tavares@gmail.com>
2022-03-13 00:47:35 -0300

    teste de posição primeiro octeto

--- cal-ipv4.py
+++ cal-ipv4.py
@@ -3,20 +3,69 @@
 # os respectivos valores são: 128 64 32 16 8 4 2 1
 # Em observação o valor posterior é a metade do antecesso
 
-
 # Aqui temos os intervalos de classes representativa
 # O valor do primeiro octeto representa a classe
 
+# "Classe A de 10.0.0.0 - 10.255.255.255"
+# "Classe B 172.16.0.0 - 172.31.255.255"
+# "Classe C 192.168.0.0 - 192.168.255.255"
 
-from turtle import clear
+from email.mime import base
 
 
-classA = "Classe A de 10.0.0.0 - 10.255.255.255"
-classB = "Classe B 172.16.0.0 - 172.31.255.255"
-classC = "Classe C 192.168.0.0 - 192.168.255.255"
+ipv4 = input(r'digite um ip valido "com pontos": ')
+# my_mask = input(r'digite uma maskara *"com pontos"*')
 
-#         8    7   6   5  4  3  2  1
+octet_ipv4 = ipv4.split('.')
+tipo_classe = ''
+
+#     =>  8    7   6   5  4  3  2  1
 octet = [128, 64, 32, 16, 8, 4, 2, 1]
+
+octet1 = int(octet_ipv4[0])
+octet2 = int(octet_ipv4[1])
+octet3 = int(octet_ipv4[2])
+octet4 = int(octet_ipv4[3])
+# --------------------------------------------------------------
+classe_oct = octet1
+soma_res = 0
+bit = []
+for index in range(7):
+    if octet[index] <= classe_oct:
+        res = classe_oct % octet[index]
+        bit.append(octet[index])
+        if res == octet[index]:
+            break
+        classe_oct = res
+print(bit)
+soma = 0
+for v in bit:
+    soma += v
+print(soma)
+# ---------------------------------------------------------------------------------------
+if octet1 > 0 and octet1 < 128:
+    if octet1 == 127:
+        print('IP  Reservado para testes internos dispositivo loopback - "localhost"')
+
+    else:
+        tipo_classe = 'A'
+        print('Classe A')
+# ---------------------------------------------------------------------------------------
+if octet1 > 127 and octet1 < 192:
+    tipo_classe = 'B'
+    print('Classe B')
+# ---------------------------------------------------------------------------------------
+if octet1 > 191 and octet1 < 224:
+    tipo_classe = 'C'
+    print('Classe C')
+# ---------------------------------------------------------------------------------------
+if octet1 > 223 and octet1 < 240:
+    tipo_classe = 'D'
+    print('Classe D')
+# ---------------------------------------------------------------------------------------
+if octet1 > 239 and octet1 < 256:
+    tipo_classe = 'E'
+    print('Classe E')
 
 for v in range(4):
     for index in range(8):
@@ -38,5 +87,5 @@ for v in range(4):
             print(f'/ {0}', end='')
 
 
-# my_ipv4 = input(r'digite um ip valido *"com pontos"*')
-# my_mask = input(r'digite uma maskara *"com pontos"*')
+# --------------------------------------------------
+# append, insert, pop, del, clear, extend, +
